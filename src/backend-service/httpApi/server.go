e078fa5ac065835e635c952559feb1af2e54d59d
GuyAfik <guyafik11@gmail.com>
2022-04-23 17:47:51 +0300

    build simple hello-world service

--- src/backend-service/httpApi/server.go
+++ src/backend-service/httpApi/server.go
@@ -1,5 +1,31 @@
 package httpApi
 
+import (
+	"context"
+	"log"
+	"net"
+
+	"github.com/Todo-Task-Management/server/helloworld_pb"
+	"google.golang.org/grpc"
+)
+
+type Server struct {
+}
+
+func (s *Server) Hello(ctx context.Context, req *helloworld_pb.HelloWorldRequest) (*helloworld_pb.HelloWorldResponse, error) {
+	return helloworld_pb.HelloWorldResponse{Message: "hello " + req.Name}, nil
+}
+
 func main() {
-	//test
+	grpcServer := grpc.NewServer()
+
+	listen, err := net.Listen("tcp", "0.0.0.0:5000")
+	if err != nil {
+		log.Fatalf("could not listen to 0.0.0.0:5000 %v", err)
+	}
+	server := Server{}
+	helloworld_pb.RegisterHelloWorldServiceServer(grpcServer, &server)
+	if err := grpcServer.Serve(listen); err != nil {
+		log.Fatalf("failed to serve: %v", err)
+	}
 }
